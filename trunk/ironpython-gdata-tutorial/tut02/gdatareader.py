0ed819023cd512ab1207bfc76ae9eae95991bc02
mark.john.rees <mark.john.rees@57c22ebd-2c1a-0410-9a7b-55d4b9090710>
2006-08-07 07:13:51 +0000

    Fix entry display to work correctly under both .NET and Mono.

--- trunk/ironpython-gdata-tutorial/tut02/gdatareader.py
+++ trunk/ironpython-gdata-tutorial/tut02/gdatareader.py
@@ -20,6 +20,16 @@ from System.Windows.Forms import AnchorStyles
 clr.AddReference("gdata.dll")
 import Google.GData.Client as GDClient
 
+# Normally we can get the text file line separator from os.linesep
+# but since I want this tutorial to work without the standard CPython
+# libraries we need to do the following:
+try:
+    import nt as os
+    os.linesep = "\r\n"
+except:
+    import posix as os
+    os.linesep = "\n"
+    
 def parse(uri, nc=None):
     '''
     Create a query and service object
@@ -35,7 +45,7 @@ def parse(uri, nc=None):
 def load_feed(feeduri):
     '''
     Attempt to load a GDATA feed passed as argument feeduri.
-    Return list of entries.
+    Return list of entry objects.
     '''
     if feeduri == "":
         return []
@@ -43,7 +53,7 @@ def load_feed(feeduri):
         feed = parse(feeduri)
         entries = []
         for entry in feed.Entries:
-            entries.append("%s\n%s\n" % (entry.Title.Text, entry.Summary.Text))
+            entries.append(entry)
         return entries
     except Exception, e:
         raise e
@@ -54,7 +64,7 @@ class GDataReaderForm(System.Windows.Forms.Form):
         self.feeduri = feeduri
         if self.feeduri is not None:
             self.gdataUriTextBox.Text = self.feeduri
-            self.gdataLoadFeed(None, None)
+            self.GdataLoadFeed(None, None)
 
     def _initgui(self):
         '''
@@ -83,7 +93,7 @@ class GDataReaderForm(System.Windows.Forms.Form):
         self.gdataRefreshButton.Anchor = (AnchorStyles.Top | AnchorStyles.Right)
         self.gdataRefreshButton.TabIndex = 1
         self.gdataRefreshButton.Text = "Refresh"
-        self.gdataRefreshButton.Click += self.gdataLoadFeed
+        self.gdataRefreshButton.Click += self.GdataLoadFeed
         # gdataUriTextBox
         self.gdataUriTextBox.Location = System.Drawing.Point(56, 18)
         self.gdataUriTextBox.Name = "gdataUriTextBox"
@@ -92,7 +102,7 @@ class GDataReaderForm(System.Windows.Forms.Form):
                                 AnchorStyles.Bottom | AnchorStyles.Top)
         self.gdataUriTextBox.TabIndex = 2
         self.gdataUriTextBox.Text = ""
-        self.gdataUriTextBox.Leave += self.gdataLoadFeed
+        self.gdataUriTextBox.Leave += self.GdataLoadFeed
         # gdataEntriesTextBox
         self.gdataEntriesTextBox.Location = System.Drawing.Point(8, 56)
         self.gdataEntriesTextBox.Multiline = True
@@ -103,6 +113,7 @@ class GDataReaderForm(System.Windows.Forms.Form):
         self.gdataEntriesTextBox.Anchor = (AnchorStyles.Left | AnchorStyles.Right |
                                 AnchorStyles.Bottom | AnchorStyles.Top)
         self.gdataEntriesTextBox.TabIndex = 3
+        self.gdataEntriesTextBox.TabStop = False
         self.gdataEntriesTextBox.Text = ""
         # statusBar
         self.statusBar.Location = System.Drawing.Point(0, 303)
@@ -131,15 +142,27 @@ class GDataReaderForm(System.Windows.Forms.Form):
         System.ComponentModel.ISupportInitialize.EndInit(self.statusBarPanelMsg)
         self.ResumeLayout(False)
 
-    def gdataLoadFeed(self, sender, args):
+    def GdataLoadFeed(self, sender, args):
         try:
-            entries = load_feed(self.gdataUriTextBox.Text)
-            self.gdataEntriesTextBox.Text = "\n".join(entries)
+            entries = []
+            for entry in load_feed(self.gdataUriTextBox.Text):
+                entries.append(self.FormatEntry(entry))
+            self.gdataEntriesTextBox.Text = os.linesep.join(entries)
         except Exception, e:
-            self.displayError(e)
+            self.DisplayError(e)
 
-    def displayError(self, e):
-        self.gdataEntriesTextBox.Text = "Unable to display feed due to following error:\n%s" % str(e)
+    def DisplayError(self, e):
+        self.gdataEntriesTextBox.Text = "Unable to display feed due to following error:%s%s" % (os.linesep, str(e))
+
+    def FormatEntry(self, entry):
+        '''
+        Return entry formatted as a number of strings delimited with the line
+        separator for the OS platform.
+        '''
+        return "%s%s%s%s" % (entry.Title.Text,
+                            os.linesep,
+                            entry.Summary.Text,
+                            os.linesep)
 
 def main(argv=sys.argv):
     System.Windows.Forms.Application.EnableVisualStyles()
