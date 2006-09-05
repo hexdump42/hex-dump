dc5a2c548b1c93bf70b5a5e8cc9e84998e14ca24
mark.john.rees <mark.john.rees@57c22ebd-2c1a-0410-9a7b-55d4b9090710>
2006-09-05 01:19:31 +0000

    Use parameters instead of string concatenation for dynamic insert statement.

--- trunk/ironpython-data-access-tutorial/tut01/load_ip2country.py
+++ trunk/ironpython-data-access-tutorial/tut01/load_ip2country.py
@@ -9,6 +9,7 @@ Version created for tutorial 1 of IronPython ADO series.
 
 License: MIT
 '''
+import sys
 import clr
 import System
 clr.AddReference("System.Data")
@@ -16,10 +17,11 @@ import System.Data
 try:
     clr.AddReference("Mono.Data.SqliteClient")
     from Mono.Data.SqliteClient import SqliteConnection as dbconnection
+    from Mono.Data.SqliteClient import SqliteParameter as dbparam
     connectstr = 'URI=file:ip2country.db,version=3'
 except:
-    clr.AddReference("System.Data.ODBCClient")
     from System.Data.Odbc import OdbcConnection as dbconnection
+    from System.Data.Odbc import OdbcParameter as dbparam
     connectstr = 'DSN=ip2country'
 
 dbcon = dbconnection(connectstr)
@@ -33,16 +35,36 @@ dbcmd = dbcon.CreateCommand()
 insert_statement=  '''
     INSERT INTO ip2country (
         ipfrom, ipto, countrycode2, countrycode3, countryname
-        ) VALUES ( %s,%s,%s,%s,%s)
+        ) VALUES ( ?,?,?,?,? )
 '''
+# Create empty parameters for insert and attach to db command
+p1 = dbparam()
+dbcmd.Parameters.Add(p1)
+p2 = dbparam()
+dbcmd.Parameters.Add(p2)
+p3 = dbparam()
+dbcmd.Parameters.Add(p3)
+p4 = dbparam()
+dbcmd.Parameters.Add(p4)
+p5 = dbparam()
+dbcmd.Parameters.Add(p5)
+
+dbcmd.CommandText = insert_statement
 
 f = open("ip-to-country.csv")
+print "Loading..."
 for line in f.readlines():
-    line = line[:-2] # remove \r\n
+    if line.endswith("\r\n"):
+        line = line[:-2] # remove \r\n
+    else:
+	line = line[:-1] # just remove \n
+    print line
     ipf, ipt, cc2, cc3, cn = re_csv.split(line)
-    sqlcmd = insert_statement % (ipf, ipt, cc2, cc3, cn)
-    print sqlcmd
-    dbcmd.CommandText = sqlcmd
+    p1.Value = ipf[1:-1]
+    p2.Value = ipt[1:-1]
+    p3.Value = cc2[1:-1]
+    p4.Value = cc3[1:-1]
+    p5.Value = cn[1:-1]
     dbcmd.ExecuteNonQuery()
 
 f.close()
